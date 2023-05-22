23154924b38388fe7b828877c4043c9b69cabe98
Adhiraj Ghosh <adhirajghosh1998@gmail.com>
2023-05-22 15:08:19 +0200

    new video style

---
+++ t2v/model.py
@@ -0,0 +1,184 @@
+from enum import Enum
+import gc
+import numpy as np
+import tomesd
+import torch
+
+from diffusers import StableDiffusionInstructPix2PixPipeline, StableDiffusionControlNetPipeline, ControlNetModel, UNet2DConditionModel
+from diffusers.schedulers import EulerAncestralDiscreteScheduler, DDIMScheduler
+from text_to_video_pipeline import TextToVideoPipeline
+
+import utils
+import os
+on_huggingspace = os.environ.get("SPACE_AUTHOR_NAME") == "PAIR"
+
+
+class ModelType(Enum):
+    Text2Video = 1,
+
+class Model:
+    def __init__(self, device, dtype, **kwargs):
+        self.device = device
+        self.dtype = dtype
+        self.generator = torch.Generator(device=device)
+        self.pipe_dict = {
+            ModelType.Text2Video: TextToVideoPipeline,
+        }
+        self.text2video_attn_proc = utils.CrossFrameAttnProcessor(
+            unet_chunk_size=2)
+
+        self.pipe = None
+        self.model_type = None
+
+        self.states = {}
+        self.model_name = ""
+
+    def set_model(self, model_type: ModelType, model_id: str, **kwargs):
+        if hasattr(self, "pipe") and self.pipe is not None:
+            del self.pipe
+            self.pipe = None
+        torch.cuda.empty_cache()
+        gc.collect()
+        safety_checker = kwargs.pop('safety_checker', None)
+        self.pipe = self.pipe_dict[model_type].from_pretrained(
+            model_id, safety_checker=safety_checker, **kwargs).to(self.device).to(self.dtype)
+        self.model_type = model_type
+        self.model_name = model_id
+
+    def inference_chunk(self, frame_ids, **kwargs):
+        if not hasattr(self, "pipe") or self.pipe is None:
+            return
+
+        prompt = np.array(kwargs.pop('prompt'))
+        negative_prompt = np.array(kwargs.pop('negative_prompt', ''))
+        latents = None
+        if 'latents' in kwargs:
+            latents = kwargs.pop('latents')[frame_ids]
+        if 'image' in kwargs:
+            kwargs['image'] = kwargs['image'][frame_ids]
+        if 'video_length' in kwargs:
+            kwargs['video_length'] = len(frame_ids)
+        if self.model_type == ModelType.Text2Video:
+            kwargs["frame_ids"] = frame_ids
+        return self.pipe(prompt=prompt[frame_ids].tolist(),
+                         negative_prompt=negative_prompt[frame_ids].tolist(),
+                         latents=latents,
+                         generator=self.generator,
+                         **kwargs)
+
+    def inference(self, split_to_chunks=False, chunk_size=8, **kwargs):
+        if not hasattr(self, "pipe") or self.pipe is None:
+            return
+
+        if "merging_ratio" in kwargs:
+            merging_ratio = kwargs.pop("merging_ratio")
+
+            # if merging_ratio > 0:
+            tomesd.apply_patch(self.pipe, ratio=merging_ratio)
+        seed = kwargs.pop('seed', 0)
+        if seed < 0:
+            seed = self.generator.seed()
+        kwargs.pop('generator', '')
+
+        if 'image' in kwargs:
+            f = kwargs['image'].shape[0]
+        else:
+            f = kwargs['video_length']
+
+        assert 'prompt' in kwargs
+        prompt = [kwargs.pop('prompt')] * f
+        negative_prompt = [kwargs.pop('negative_prompt', '')] * f
+
+        frames_counter = 0
+
+        # Processing chunk-by-chunk
+        if split_to_chunks:
+            chunk_ids = np.arange(0, f, chunk_size - 1)
+            result = []
+            for i in range(len(chunk_ids)):
+                ch_start = chunk_ids[i]
+                ch_end = f if i == len(chunk_ids) - 1 else chunk_ids[i + 1]
+                frame_ids = [0] + list(range(ch_start, ch_end))
+                self.generator.manual_seed(seed)
+                print(f'Processing chunk {i + 1} / {len(chunk_ids)}')
+                result.append(self.inference_chunk(frame_ids=frame_ids,
+                                                   prompt=prompt,
+                                                   negative_prompt=negative_prompt,
+                                                   **kwargs).images[1:])
+                frames_counter += len(chunk_ids)-1
+                if on_huggingspace and frames_counter >= 80:
+                    break
+            result = np.concatenate(result)
+            return result
+        else:
+            self.generator.manual_seed(seed)
+            return self.pipe(prompt=prompt, negative_prompt=negative_prompt, generator=self.generator, **kwargs).images
+
+    def process_text2video(self,
+                           prompt,
+                           model_name="dreamlike-art/dreamlike-photoreal-1.0",
+                           motion_field_strength_x=12,
+                           motion_field_strength_y=12,
+                           t0=44,
+                           t1=47,
+                           n_prompt="",
+                           chunk_size=8,
+                           video_length=8,
+                           merging_ratio=0.5,
+                           seed=0,
+                           resolution=512,
+                           fps=2,
+                           use_cf_attn=True,
+                           use_motion_field=True,
+                           smooth_bg=False,
+                           smooth_bg_strength=0.4,
+                           path=None):
+        print("Module Text2Video")
+        if self.model_type != ModelType.Text2Video or model_name != self.model_name:
+            print("Model update")
+            unet = UNet2DConditionModel.from_pretrained(
+                model_name, subfolder="unet")
+            self.set_model(ModelType.Text2Video,
+                           model_id=model_name, unet=unet)
+            self.pipe.scheduler = DDIMScheduler.from_config(
+                self.pipe.scheduler.config)
+            if use_cf_attn:
+                self.pipe.unet.set_attn_processor(
+                    processor=self.text2video_attn_proc)
+        self.generator.manual_seed(seed)
+
+        added_prompt = "high quality, HD, 32K, trending on artstation, high focus, dramatic lighting, ultra-realistic, DSLR photography"
+        negative_prompts = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer difits, cropped, worst quality, low quality, deformed body, bloated, ugly, unrealistic, nude, naked'
+
+        prompt = prompt.rstrip()
+        if len(prompt) > 0 and (prompt[-1] == "," or prompt[-1] == "."):
+            prompt = prompt.rstrip()[:-1]
+        prompt = prompt.rstrip()
+        prompt = prompt + ", "+added_prompt
+        if len(n_prompt) > 0:
+            negative_prompt = n_prompt
+        else:
+            negative_prompt = None
+
+        result = self.inference(prompt=prompt,
+                                video_length=video_length,
+                                height=resolution,
+                                negative_prompt=negative_prompts,
+                                width=resolution,
+                                num_inference_steps=50,
+                                guidance_scale=7.5,
+                                guidance_stop_step=1.0,
+                                t0=t0,
+                                t1=t1,
+                                motion_field_strength_x=motion_field_strength_x,
+                                motion_field_strength_y=motion_field_strength_y,
+                                use_motion_field=use_motion_field,
+                                smooth_bg=smooth_bg,
+                                smooth_bg_strength=smooth_bg_strength,
+                                seed=seed,
+                                output_type='numpy',
+                                merging_ratio=merging_ratio,
+                                split_to_chunks=True,
+                                chunk_size=chunk_size,
+                                )
+        return utils.create_video(result, fps, path=path, watermark=None)
---
+++ t2v/video.py
@@ -0,0 +1,62 @@
+import torch
+from model import Model
+import argparse
+import fire
+import numpy as np
+import os
+from utils import *
+from transformers import pipeline
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a detector')
+    parser.add_argument('--gpu', help='GPU id', type=int, default=0)
+    parser.add_argument('--fps', help='frame rate of gif', type=float, default=12.5)
+    parser.add_argument('--song_path', help='song file path', default = './all_star_mod2')
+    parser.add_argument('--result_dir', help='name of folder for outputs',
+                        default='./results')
+    parser.add_argument('--name', help='name of song folder/gif/log',
+                        default='song1_extend')
+    parser.add_argument('--extend', help='use prompt extend or not. Type --extend or --no-extend', default=False,
+                        action=argparse.BooleanOptionalAction)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    song_name = args.song_path.split('/')[-1]
+    output_dir = os.path.join(args.result_dir, song_name,args.name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    video_path = os.path.join(args.result_dir, song_name, args.name + ".mp4")
+    print("Loading full song")
+    full_song = song_to_list(args.song_path)
+
+    model_name = ['dreamlike-art/dreamlike-photoreal-2.0', 'runwayml/stable-diffusion-v1-5']
+    model = Model(device=args.gpu, dtype=torch.float16)
+    text_pipe = pipeline('text-generation', model='./prompt-extend', device=args.gpu, max_new_tokens=30)
+
+    params = {"t0": 44, "t1": 47, "motion_field_strength_x": 12, "motion_field_strength_y": 12, "video_length": 30}
+
+    line_no = 1
+    for stanza in full_song:
+        for line in stanza:
+            if args.extend:
+                line = text_pipe(line + ',', num_return_sequences=1)[0]["generated_text"]
+            print(line)
+            out_path = os.path.join(output_dir, str(line_no)+".mp4")
+            print("Saving to ", out_path)
+            model.process_text2video(line, model_name = model_name[0], fps = args.fps, path = out_path, seed = np.random.randint(100,999),**params)
+            line_no = line_no+1
+
+    print("Merging video")
+    merge_mp4(folder_path = output_dir, output_path=video_path)
+
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
+
