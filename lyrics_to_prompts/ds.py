6b91cab7d205da51204c81feb29593f91f4ebbd9
Hazel1994 <hasan.karezan@gmail.com>
2023-05-23 10:07:53 +0200

    update + fix some bugs

--- lyrics_to_prompts/ds.py
+++ lyrics_to_prompts/ds.py
@@ -5,13 +5,18 @@ import numpy as np
 import json
 import yaml
 import re
+from tqdm import  tqdm
 
-def starts_with_1_to_50(string):
-    regex = r"^(?:[1-9]|[1-4]\d|50)"
-    return bool(re.match(regex, string))
+pattern = r'^([1-9]|[1-4][0-9]|50)\.\s'  # Regular expression pattern
+def check_prompt_format(current_line):
+    return bool(re.match(pattern, current_line))
+
+# def starts_with_1_to_50(string):
+#     regex = r"^(?:[1-9]|[1-4]\d|50)"
+#     return bool(re.match(regex, string))
 
 lyric_path='/graphics/scratch2/staff/Hassan/genius_crawl/'
-prompt_path = '/graphics/scratch2/staff/Hassan/chatgpt_data/'
+prompt_path = '/graphics/scratch2/staff/Hassan/chatgpt_data_v2.0/'
 ds_path = "../SongAnimator.csv"
 data={}
 file = lyric_path+"dataset_50.pickle"
@@ -26,46 +31,49 @@ my_song = []
 my_lyric = []
 my_prompt = []
 idx = 1
-for artist in file.keys():
-    for songs in range(len(file[artist])):
+size_threshold=1000 # bytes
 
-        full_song = file[artist][songs]['title']
-        song = full_song.split(' by\xa0')[0]
-        # print(song)
-        # print(artist)
-        lyric = file[artist][songs]['lyrics']
+for artist,songs  in tqdm(file.items()):
 
-        # check if prompts exist
-        if os.path.exists(os.path.join(prompt_path, artist, full_song)) == False or os.stat(
-                os.path.join(prompt_path, artist, full_song)).st_size == 0:
-            continue
-        else:
-            with open(os.path.join(prompt_path, artist, full_song), 'r') as f:
-                gpt = yaml.load(f.readlines()[0])
+    #check if the artist exist
+    if os.path.exists(os.path.join(prompt_path, artist)):
 
-        gpt_id = gpt['id'].split('chatcmpl-')[1]
-        prompts = gpt['choices'][0]['message']['content'].split('\n')
+        for song in songs:
 
-        # Section for debugging
-        # 1. Removing cases where prompts are nonsensical. I chose 3 because by definition, the shortest line prompt has a number from 0-9 followed by '. ', so 3 characters.
-        # 2. Fix case where there are more prompts than lyrics
-        # 3. Exclude songs that were not processed
-        # 4. If there exist lines that aren't part of the song at the end
+            full_title = song['title']
+            lyric = song['lyrics']
 
-        prompts = [x for x in prompts if starts_with_1_to_50(x) == True]
+            # check if prompts exist
+            if os.path.exists(os.path.join(prompt_path, artist, full_title)) == False or os.stat(
+                    os.path.join(prompt_path, artist, full_title)).st_size < size_threshold:
+                continue
+            else:
+                with open(os.path.join(prompt_path, artist, full_title), 'r') as f:
+                    gpt = yaml.load(f.readlines()[0],Loader=yaml.Loader)
 
-        if not prompts:
-            continue
+            gpt_id = gpt['id'].split('chatcmpl-')[1]
+            prompts = gpt['choices'][0]['message']['content'].split('\n')
 
-        for i in range(min(len(prompts), len(lyric))):
-            line = lyric[i]
-            my_id.append(idx)
-            my_gpt_id.append(gpt_id)
-            my_artist.append(artist)
-            my_song.append(song)
-            my_lyric.append(line)
-            my_prompt.append(prompts[i].split('.')[1])
-            idx = idx + 1
+            # Section for debugging
+            # 1. Removing cases where prompts are nonsensical. I chose 3 because by definition, the shortest line prompt has a number from 0-9 followed by '. ', so 3 characters.
+            # 2. Fix case where there are more prompts than lyrics
+            # 3. Exclude songs that were not processed
+            # 4. If there exist lines that aren't part of the song at the end
+
+            prompts = [x for x in prompts if check_prompt_format(x)]
+
+            if not prompts:
+                continue
+
+            for i in range(min(len(prompts), len(lyric))):
+                line = lyric[i]
+                my_id.append(idx)
+                my_gpt_id.append(gpt_id)
+                my_artist.append(artist)
+                my_song.append(full_title)
+                my_lyric.append(line)
+                my_prompt.append(prompts[i].split('.')[1])
+                idx = idx + 1
 
 lyric_prompt['id'] = my_id
 lyric_prompt['gpt_id'] = my_gpt_id
