b0cec1ba0b62c373475f8a7723fa79ae3b36ce0d
Hassan <hshahmohammadi86@r2s-l1.sdn>
2023-05-28 17:51:53 +0200

    update config

--- lyrics_to_prompts/training_ml.py
+++ lyrics_to_prompts/training_ml.py
@@ -110,7 +110,7 @@ def main():
     # model.load_state_dict(checkpoint['state_dict'])
     # print('checkpoint loaded')
     #trainer=Trainer(accelerator='gpu', devices=8, callbacks=[checkpoint_callback, early_stop], logger=tb_logger,max_epochs=max_epochs,strategy='ddp')
-    trainer = Trainer(accelerator='gpu', devices=8, callbacks=[checkpoint_callback, early_stop], logger=tb_logger,    max_epochs=max_epochs, limit_train_batches=10, limit_val_batches=10)
+    trainer = Trainer(accelerator='gpu', devices=8, callbacks=[checkpoint_callback, early_stop], logger=tb_logger,    max_epochs=max_epochs,strategy='ddp')
     trainer.fit(model)
 
 if __name__ == "__main__":
