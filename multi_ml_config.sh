e005a07ab1bc309acc788f8b6773690b1d5ceb85
Hazel1994 <hasan.karezan@gmail.com>
2023-05-29 20:04:11 +0200

    update mutlticonfig for contenxt size ablation

--- multi_ml_config.sh
+++ multi_ml_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=gpt2_lr                  # Job name
+#SBATCH --job-name=gpt2_cxs                  # Job name
 #SBATCH --partition=a100                # partition
 #SBATCH --gres=gpu:8                  # type and number of gpus
 #SBATCH --mem=50G                           # Memory pool for all cores (see also --mem-per-cpu)
@@ -17,11 +17,17 @@ conda activate /mnt/lustre/lensch/hshahmohammadi86/.conda/envs/env
 
 cd /mnt/lustre/lensch/hshahmohammadi86/projects/SongAnimator/lyrics_to_prompts/
 
-lrs=(2e-5 5e-5 2e-4 5e-4)
-# Get the learning rate for the current job index
-lr=${lrs[$SLURM_ARRAY_TASK_ID]}
+#lrs=(2e-5 5e-5 2e-4 5e-4)
 
-srun  python training_ml.py --ml 1  --batch_size 164 --learning_rate "$lr" --context_length 0
+# Get the learning rate for the current job index
+#lr=${lrs[$SLURM_ARRAY_TASK_ID]}
+context_sizes=(1 3 5 7)
+cxs=${context_sizes[$SLURM_ARRAY_TASK_ID]}
+
+# Get the learning rate for the current job index
+#lr=${lrs[$SLURM_ARRAY_TASK_ID]}
+
+srun  python training_ml.py --ml 1  --batch_size 50 --learning_rate 2e-4 --context_length "$cxs"
 
 #echo '---------------- Status of this machine: ----------------'
 #nvidia-smi
